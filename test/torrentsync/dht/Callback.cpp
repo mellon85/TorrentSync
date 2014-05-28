e741d73245529a169be85798af932559ed7d743d
Dario Meloni <mellon85@gmail.com>
2014-05-28 14:02:42 +0200

    Added Callback test empty code

---
+++ test/torrentsync/dht/Callback.cpp
@@ -0,0 +1,24 @@
+#include <boost/test/unit_test.hpp>
+#include <sstream>
+
+#include <torrentsync/dht/Callback.h>
+#include <test/torrentsync/dht/CommonNodeTest.h>
+
+BOOST_AUTO_TEST_SUITE(torrentsync_dht_Callback);
+
+using namespace torrentsync::dht;
+
+BOOST_AUTO_TEST_CASE(constructor_destructor)
+{
+    const std::string type = "q";
+
+    Callback call(Callback::callback(),
+        type,
+        Callback::filterPeer(),
+        Callback::filterTransactionID());
+
+    BOOST_REQUIRE_EQUAL(false,call.isOld());
+}
+
+BOOST_AUTO_TEST_SUITE_END();
+
