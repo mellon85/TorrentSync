1a5d8cc54fb65cbeb51278f2a1441a267e1bdea8
Dario Meloni <mellon85@gmail.com>
2014-06-04 18:36:27 +0200

    YouCompleteMe configurations

--- .ycm_extra_conf.py
+++ .ycm_extra_conf.py
@@ -54,7 +54,6 @@ flags = [
 #
 # Most projects will NOT need to set this to anything; you can just change the
 # 'flags' list of compilation flags. Notice that YCM itself uses that approach.
-#compilation_database_folder = '/home/dario/Desktop/TorrentSync'
 compilation_database_folder = ''
 
 if os.path.exists( compilation_database_folder ):
