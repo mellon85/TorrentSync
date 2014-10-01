b42ffee66dff3468b5d4e4f47e638b2f27ffd63e
Dario Meloni <mellon85@gmail.com>
2014-10-01 13:22:55 +0200

    Added documentation to TokenManager.h

--- torrentsync/dht/TokenManager.h
+++ torrentsync/dht/TokenManager.h
@@ -5,15 +5,20 @@ namespace torrentsync
 {
 namespace dht
 {
-
 namespace TokenManager
 {
 
-utils::Buffer getToken( const boost::asio::ip::udp::endpoint& );
+//! @param endpoint the endpoint to generate the token for
+//! @return a Token created based on the currently valid seed value
+utils::Buffer getToken( const boost::asio::ip::udp::endpoint& endpoint);
 
-bool isTokenValid( const utils::Buffer&, const boost::asio::ip::udp::endpoint& );
+//! @param token    Client provided token
+//! @param endpoint Client endpoint
+//! @return true if the token is valid based on the current and the previous still valid seed values
+bool isTokenValid(
+    const utils::Buffer& token,
+    const boost::asio::ip::udp::endpoint& endpoint);
 
 } /* TokenManager */ 
-
 } /* dht */ 
 } /* torrentsync */
